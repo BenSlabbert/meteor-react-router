0ce652b675026831c354076c734620122564f8fd
Ben Slabbert <ben@hex.co.za>
2017-12-22 19:06:12 +0200

    sidebar example from react-router site: https://reacttraining.com/react-router/web/example/sidebar

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SidebarExample from './SidebarExample';
+
+export const App = () => {
+    return (
+        <SidebarExample/>
+    );
+};
+
+export default App;
